a867c82b9d0f1b66820653bd43260fe2e5bcd668
Shaan Kumar <shaan123rock@gmail.com>
2022-11-14 18:37:25 +0530

    All test done and result updated

--- quic-nginx/Test5/script.py
+++ quic-nginx/Test5/script.py
@@ -8,9 +8,9 @@ packet = packetCollection[0]
 
 # Changes applied
 
-packet[IP].ihl = 10000
-packet[UDP].len = 10
-packet[IP].id = 2120
+packet[IP].ihl = 10000      # Changed ihl variable in IP layer
+packet[UDP].len = 10        # Reduced header length in UDP layer
+packet[IP].id = 2120        # Changed IP's Id
 
 packet.show()
 
